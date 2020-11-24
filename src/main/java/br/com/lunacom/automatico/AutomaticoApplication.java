b6771eb2f2f1e9f2f66117209c959e86fbe158bd
alexpereira2017 <alexpereira2004@gmail.com>
2020-11-23 22:45:30 -0300

    Projeto inicial

---
+++ src/main/java/br/com/lunacom/automatico/AutomaticoApplication.java
@@ -0,0 +1,13 @@
+package br.com.lunacom.automatico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomaticoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/lunacom/automatico/AutomaticoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lunacom.automatico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
