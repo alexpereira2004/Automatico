8af10f69af56b634a156f802cdd0afc53ffafb33
alexpereira2017 <alexpereira2004@gmail.com>
2020-11-25 16:33:41 -0300

    Feign: Dependencias

--- src/main/java/br/com/lunacom/automatico/AutomaticoApplication.java
+++ src/main/java/br/com/lunacom/automatico/AutomaticoApplication.java
@@ -2,8 +2,12 @@ package br.com.lunacom.automatico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
+@EnableFeignClients(basePackages = {"br.com.lunacom.*"})
 public class AutomaticoApplication {
 
     public static void main(String[] args) {
