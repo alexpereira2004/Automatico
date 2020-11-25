ba90c60a025db459fb7640f328d85e8c9730d50f
alexpereira2017 <alexpereira2004@gmail.com>
2020-11-25 16:38:01 -0300

    Feign: Chamada para teste Endpoint /parametrizacao [GET]

---
+++ src/main/java/br/com/lunacom/automatico/service/AutomaticoService.java
@@ -0,0 +1,23 @@
+package br.com.lunacom.automatico.service;
+
+import br.com.lunacom.automatico.client.LeitorDeIndicesClient;
+import br.com.lunacom.automatico.domain.dto.CotacaoAtivoDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class AutomaticoService {
+
+    private final LeitorDeIndicesClient leitorDeIndicesClient;
+
+    public void teste() {
+        final ResponseEntity<List<CotacaoAtivoDto>> response = leitorDeIndicesClient.find("TSLA34",
+                "01/10/2020",
+                "01/11/2020");
+        return;
+    }
+}
