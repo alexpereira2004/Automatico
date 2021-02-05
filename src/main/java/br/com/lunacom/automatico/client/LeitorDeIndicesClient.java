73a6274f5652e5cff0793a91135b5b29d1bba8ec
alexpereira2017 <alexpereira2004@gmail.com>
2021-02-05 00:18:54 -0300

    Novo atributo do servico de scraping

--- src/main/java/br/com/lunacom/automatico/client/LeitorDeIndicesClient.java
+++ src/main/java/br/com/lunacom/automatico/client/LeitorDeIndicesClient.java
@@ -11,15 +11,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Component
-@FeignClient(name = "leitor-de-indices", url = "localhost:8081")
+
+@FeignClient(
+        name = "leitor-de-indices",
+        url = "localhost:8765",
+        path = "/leitor-de-indices/scraping")
 //@FeignClient(name = "hr-worker", path = "/workers")
 public interface LeitorDeIndicesClient {
 
     @RequestMapping(method= RequestMethod.GET, path = "/ativo/todos-com-cotacao")
     ResponseEntity<List<AtivoResumoDto>> findTodosComCotacao();
 
-    @RequestMapping(method= RequestMethod.GET, path = "/scraping/historico/ativos/")
-    ResponseEntity<Void> scrapingHistoricoAtivos(@RequestParam("ativos") List<String> listaAtivos,
-                                                                 @RequestParam("inicio") String inicio,
-                                                                 @RequestParam("invisivel") Boolean invisivel);
+    @RequestMapping(method= RequestMethod.GET, path = "/historico/ativos/")
+    ResponseEntity<Void> scrapingHistoricoAtivos(
+            @RequestParam("ativos") List<String> listaAtivos,
+            @RequestParam("inicio") String inicio,
+            @RequestParam("invisivel") Boolean invisivel,
+            @RequestParam("site") String site
+    );
 }
