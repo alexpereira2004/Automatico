73a3cca154da288c090c580f7ae4f06be1659fd9
alexpereira2004 <alexpereira2004@gmail.com>
2022-02-06 14:28:44 -0300

    Regex para ler os dados referentes aos dias uteis

--- src/main/java/br/com/lunacom/automatico/service/DiaUtilService.java
+++ src/main/java/br/com/lunacom/automatico/service/DiaUtilService.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,7 +18,7 @@ public class DiaUtilService {
 
     private final DiaUtilRepository repository;
 //    public static final String REGEX = "<div id=\"(\\d)\" class=\"cellcalendarYearStats (red|gray|white)\"> \\s\\n.*\\n.*\\n.*<div class=\"info\">(\\d)<\\/div>\\n(:?\\t\\t<div class=\"info\"><\\/div>\\s){3}\\t\\t<div class=\"info\">(\\d+\\/\\d+\\/\\d+)<\\/div>\\n\\t\\t<div class=\"info\">(.*)<\\/div>";
-    public static final String REGEX = "<div id=\"(\\d)\" class=\"cellcalendarYearStats (red|gray|white)\"> \\s\\r\\n.*\\r\\n.*\\r\\n.*<div class=\"info\">(\\d)<\\/div>\\r\\n(:?\\t\\t<div class=\"info\"><\\/div>\\r\\s){3}\\t\\t<div class=\"info\">(\\d+\\/\\d+\\/\\d+)<\\/div>\\r\\n\\t\\t<div class=\"info\">(.*)<\\/div>";
+    public static final String REGEX = "<div id=\"(\\d*)\" class=\"cellcalendarYearStats (red|gray|white)(\" .*)*\"> \\s\\r\\n.*\\r\\n.*\\r\\n.*<div class=\"info\">(\\d*)<\\/div>\\r\\n(:?\\t\\t<div class=\"info\"( .*)*><\\/div>\\r\\s){3}\\t\\t<div class=\"info\">(\\d+\\/\\d+\\/\\d+)<\\/div>\\r\\n\\t\\t<div class=\"info\">(.*)<\\/div>";
 
     public List<DiaUtil> parseFromDiasUteisCom(String request) {
         List<DiaUtil> list = new ArrayList<>();
@@ -27,13 +26,14 @@ public class DiaUtilService {
         Matcher matcher = pattern.matcher(request);
         while (matcher.find()) {
             final DiaUtil entity = DiaUtil.builder()
-                    .dia(DataUtil.dataBrParaLocalDate(matcher.group(5)))
+                    .dia(DataUtil.dataBrParaLocalDate(matcher.group(7)))
                     .dia_ano(Integer.valueOf(matcher.group(1)))
                     .tipo(TipoDiaEnum.fromFlag(matcher.group(2)))
-                    .feriado(matcher.group(6))
+                    .feriado(matcher.group(8))
                     .build();
             list.add(entity);
         }
-        return Arrays.asList(new DiaUtil());
+        List<DiaUtil> repoResult = repository.saveAll(list);
+        return repoResult;
     }
 }
