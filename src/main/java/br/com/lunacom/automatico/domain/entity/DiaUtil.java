5973f6c81a975c28e25d4361faed690bc2f9e551
alexpereira2004 <alexpereira2004@gmail.com>
2022-02-06 21:41:51 -0300

    Chave primaria da tabela dia_util passa a ser o dia

--- src/main/java/br/com/lunacom/automatico/domain/entity/DiaUtil.java
+++ src/main/java/br/com/lunacom/automatico/domain/entity/DiaUtil.java
@@ -7,8 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -21,8 +19,6 @@ import java.time.LocalDate;
 public class DiaUtil implements Serializable {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Integer id;
     private LocalDate dia;
     private Integer  dia_ano;
     private Integer  semana;
