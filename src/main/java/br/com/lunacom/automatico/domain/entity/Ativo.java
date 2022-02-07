88610b8aba29653db174c02b560e91f324084603
alexpereira2004 <alexpereira2004@gmail.com>
2022-02-07 10:58:38 -0300

    Estrutura das acoes que sigo

---
+++ src/main/java/br/com/lunacom/automatico/domain/entity/Ativo.java
@@ -0,0 +1,25 @@
+package br.com.lunacom.automatico.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Builder
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ativo implements Serializable {
+
+    @Id
+    private String codigo;
+    private String nome;
+    private LocalDateTime ultimaAtualizacao;
+    private Boolean seguindo;
+}
