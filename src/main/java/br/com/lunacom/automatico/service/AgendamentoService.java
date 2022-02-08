3e18e44efc4fa227dce50a31934ed807097eeabb
alexpereira2004 <alexpereira2004@gmail.com>
2022-02-08 00:36:04 -0300

    Estrutura das acoes que sigo  - Refatoracao do metodo unico para outros menores

--- src/main/java/br/com/lunacom/automatico/service/AgendamentoService.java
+++ src/main/java/br/com/lunacom/automatico/service/AgendamentoService.java
@@ -29,22 +29,41 @@ public class AgendamentoService {
     }
 
     public Instant definirTempoParaProximoDisparo(Optional<Date> lastCompletionTime) {
+        LocalDateTime ultimaExecucao = parseDataUltimaExecucao(lastCompletionTime);
+        Agenda agenda = getAgendaNoBancoDeDados();
+        LocalDateTime proximaExecucaoCalculada = calcularProximaExecucao(ultimaExecucao, agenda);
+        proximaExecucaoCalculada = calcularProximaExecucaoQuandoAtualForMaiorQueDataAgenda
+                (ultimaExecucao, agenda, proximaExecucaoCalculada);
+        return proximaExecucaoCalculada.toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    private LocalDateTime parseDataUltimaExecucao(Optional<Date> lastCompletionTime) {
         Date date = lastCompletionTime.orElseGet(Date::new);
 
         LocalDateTime ultimaExecucao = date
                 .toInstant().atZone(ZoneId.of("America/Sao_Paulo"))
                 .toLocalDateTime();
+        return ultimaExecucao;
+    }
 
+    private Agenda getAgendaNoBancoDeDados() {
         Optional<Agenda> agendaOptional = repository.findByDia(DiaSemanaEnum.TODOS);
         Agenda agenda = agendaOptional.get();
+        return agenda;
+    }
 
+    private LocalDateTime calcularProximaExecucao(LocalDateTime ultimaExecucao, Agenda agenda) {
         LocalDateTime proximaExecucaoCalculada = ultimaExecucao.plusMinutes(agenda.getPeriodicidade());
+        return proximaExecucaoCalculada;
+    }
 
+    private LocalDateTime calcularProximaExecucaoQuandoAtualForMaiorQueDataAgenda(LocalDateTime ultimaExecucao,
+                                                                                  Agenda agenda,
+                                                                                  LocalDateTime proximaExecucaoCalculada) {
         if (proximaExecucaoCalculada.toLocalTime().isAfter(agenda.getFim())) {
             LocalDate proximoDiaUtil = dataUtil.proximoDiaUtil(ultimaExecucao.toLocalDate());
             proximaExecucaoCalculada = LocalDateTime.of(proximoDiaUtil, agenda.getInicio());
         }
-
-        return proximaExecucaoCalculada.toInstant(ZoneOffset.of("-03:00"));
+        return proximaExecucaoCalculada;
     }
 }
