bb52e8e81bec277ccacc1f2a228f5a4b68904066
alexpereira2004 <alexpereira2004@gmail.com>
2022-02-08 00:14:23 -0300

    Calculo do dia util - Precisa integrar com o banco de dados

--- src/main/java/br/com/lunacom/automatico/util/DataUtil.java
+++ src/main/java/br/com/lunacom/automatico/util/DataUtil.java
@@ -1,19 +1,25 @@
 package br.com.lunacom.automatico.util;
 
-import lombok.NoArgsConstructor;
+import br.com.lunacom.automatico.domain.entity.DiaUtil;
+import br.com.lunacom.automatico.enumeration.TipoDiaEnum;
+import br.com.lunacom.automatico.repository.DiaUtilRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
+import java.util.Optional;
 
 @Component
-@NoArgsConstructor
+@AllArgsConstructor
 public class DataUtil {
     private static String ANO_MES_DIA = "yyyyMMdd";
     private static String ANO_MES_DIA_HIFEN = "yyyy-MM-dd";
     private static String DIA_MES_ANO_BARRA = "dd/MM/yyyy";
+
+    private final DiaUtilRepository diaUtilRepository;
 
     public static String localDateParaDataBr(LocalDate origem) {
         if (Objects.isNull(origem)) {
@@ -26,6 +32,23 @@ public class DataUtil {
     public static LocalDate dataBrParaLocalDate(String origem) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DIA_MES_ANO_BARRA);
         return LocalDate.parse(origem, formatter);
+    }
+
+    public LocalDate proximoDiaUtil(LocalDate diaReferencia) {
+        Boolean cond = false;
+        DiaUtil diaUtil = null;
+        do {
+            LocalDate proximoDia = Objects.isNull(diaUtil) ? diaReferencia : diaUtil.getDia();
+            diaUtil = proximoDia(proximoDia);
+            cond = diaUtil.getTipo().equals(TipoDiaEnum.NORMAL);
+        } while (!cond);
+        return diaUtil.getDia();
+    }
+
+    private DiaUtil proximoDia(LocalDate diaReferencia) {
+        Optional<DiaUtil> optional = diaUtilRepository.findByDia(diaReferencia.plusDays(1l));
+        return optional
+                .orElseThrow(() -> new IllegalArgumentException(("Agenda não existe")));
     }
 
     public LocalDate dataAgora() {
